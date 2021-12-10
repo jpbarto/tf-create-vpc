89b91b1b9b850378470d35af9ce8550dead85fc7
jpbarto <jason.p.barto@gmail.com>
2021-12-10 02:38:29 +0000

    fixed subnet addresses and enabled dns resolution

--- main.tf
+++ main.tf
@@ -18,11 +18,13 @@ module "vpc" {
 
   azs              = ["${data.aws_region.current.name}a", "${data.aws_region.current.name}b", "${data.aws_region.current.name}c"]
   database_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  private_subnets  = ["10.0.10.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+  private_subnets  = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
   public_subnets   = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
+  enable_nat_gateway   = true
+  enable_vpn_gateway   = false
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = {
     Terraform   = "true"
